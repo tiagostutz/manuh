ce52c0757d8b4b9c4151df7304105d8c0dde6c38
tiagostutz <tiagostutz@bb.com.br>
2016-03-22 09:45:18 -0300

    started the subscription function.

--- manuh.js
+++ manuh.js
@@ -30,8 +30,8 @@ module.exports = {
     topicsTree : {},
 
     _hasSpecialWildcard : function(str) {
-      return str.indexOf(this.multiLevelWildCard)!=-1
-          || str.indexOf(this.singleLevelWildCard)!=-1
+        return str.indexOf(this.multiLevelWildCard)!=-1
+        || str.indexOf(this.singleLevelWildCard)!=-1
     },
 
     _createTopic: function(_name, _parent) {
@@ -40,92 +40,105 @@ module.exports = {
           parent : _parent,
           subscriptions : [],
           addSubscription: function(onMessageReceived) {
-            subscriptions.push({onMessageReceived : onMessageReceived});
+              subscriptions.push({onMessageReceived : onMessageReceived});
           }
         }
         if (_parent) {
-          _parent[_name] = topic;
+            _parent[_name] = topic;
         }
 
         return topic;
     },
 
     //returns an array with all matched topics
-    _resolveTopicsByPath : function(topicPath, topicNode) {
+    _resolveTopicsByPathRegex : function(topicPath, topicNode) {
       var arrTopics = [];
       if (topicNode == null) {
-        var topicNode = this.topicsTree;
+          var topicNode = this.topicsTree;
       }
       var idxHash = topicPath.indexOf("/");
 
       var firstLevelName = topicPath;
       if (idxHash!=-1) {
-        var firstLevelName = topicPath.substring(0, topicPath.indexOf("/"));
+          var firstLevelName = topicPath.substring(0, topicPath.indexOf("/"));
       }
       if (firstLevelName.trim().length == 0) {
-        return null;
+          return null;
       }
 
       if (!topicNode.hasOwnProperty(firstLevelName)) {
-        topicNode[firstLevelName] = this._createTopic(firstLevelName, topicNode);
+          topicNode[firstLevelName] = this._createTopic(firstLevelName, topicNode);
       }
 
       arrTopics.push(topicNode[firstLevelName]);
       if (idxHash!=-1) {
-        var topics = this._resolveTopicsByPath(topicPath.substring(idxHash+1), topicNode[firstLevelName]);
-        arrTopics = arrTopics.concat(topics);
+          var topics = this._resolveTopicsByPathRegex(topicPath.substring(idxHash+1), topicNode[firstLevelName]);
+          arrTopics = arrTopics.concat(topics);
       }
 
       return arrTopics;
     },
 
-    _evaluateTopics : function(topicPath) {
+    _resolveTopic : function(topicPath) {
 
       var arrMatchedTopics = [];
       if (!this._hasSpecialWildcard(topicPath)) {
-        var arrTopics = this._resolveTopicsByPath(topicPath);
-        arrMatchedTopics.push(arrTopics[arrTopics.length-1]); //return the last topic found, that will be the last one on the path
-        return arrMatchedTopics;
+          var arrTopics = this._resolveTopicsByPathRegex(topicPath);
+          arrMatchedTopics.push(arrTopics[arrTopics.length-1]); //return only the last topic found, that will be the last one on the path
       }else{
 
         var idxHash = topicPath.indexOf("/");
 
         var firstLevelName = topicPath;
         if (idxHash != -1) {
-          firstLevelName = topicPath.substring(0, topicPath.indexOf("/"));
+            firstLevelName = topicPath.substring(0, topicPath.indexOf("/"));
         }
       }
-      },
+
+      return arrMatchedTopics;
+    },
 
 
 
     publish: function(topicPath, message) {
-      var topicToPublish = null;
+        var topicsToPublish = null;
 
-      if (!this._hasSpecialWildcard(topicPath)) {
-        topicToPublish = this._resolveTopicsByPath(topicPath);
+        if (!this._hasSpecialWildcard(topicPath)) {
+            topicsToPublish = this._resolveTopic(topicPath);
 
-      }else{ //if the path has a wildcard that needs to be evaluated
+        }else{ //if the path has a wildcard that needs to be evaluated
+            throw {msg: 'Error to publish message on topic because the topic name (path) provided has invalid characters. Note: you cannot publish using wildcards like you can use on subscriptions.'};
+        }
+        if (topicsToPublish.length > 1) {
+            throw {msg: 'Error to publish message on topic because there were found more than 1 topic for the provied topicPath. You can publish only to one topic. Check if there are duplicated topic names.'};
+        }
 
-        throw {msg: 'Erro ao publicar no topico'};
-      }
-
-      for(var k=0; k<topicToPublish.subscriptions; k++) {
-
-        //invoke the callbacks asynchronously and with a closed scope
-        var subscription = topicToPublish[i].subscriptions[k];
-        (function(subsc) {
-          var _subsc = subsc;
-          setTimeout(function() {
-            _subsc.onMessageReceived(message);
-          }, 0);
-        })(subscription);
-      }
+        for(var k=0; k<topicsToPublish[0].subscriptions; k++) {
+            //invoke the callbacks asynchronously and with a closed scope
+            var subscription = topicsToPublish[0].subscriptions[k];
+            (function(subsc) {
+              var _subsc = subsc;
+              setTimeout(function() {
+                _subsc.onMessageReceived(message);
+              }, 0);
+            })(subscription);
+        }
 
     },
 
-    subscribe: function(topicNameRegex) {
+    subscribe: function(topicNameRegex, onMessageReceived) {
+      if (!onMessageReceived) {
+          throw {msg: 'Error subscribing to `' + topicNameRegex + '` because no `onMessageReceived` callback function was provided.'};
+      }
 
+      var topicsToSubscribe = null;
+
+      if (!this._hasSpecialWildcard(topicNameRegex)) {
+          topicsToPublish = this._resolveTopicsByPathRegex(topicNameRegex);
+
+      }else{ //if the path has a wildcard that needs to be evaluated
+
+      }
     }
 
 }
--- manuh.tests.js
+++ manuh.tests.js
@@ -3,143 +3,175 @@ var assert = require('assert');
 
 describe('manuh client-side lightweight topic infrastructure', function() {
 
-  beforeEach(function() {
-    manuh.topicsTree = {}; //manual reset
-  });
-
-  describe('topic creation', function() {
-    describe('manuh._createTopic()', function() {
-
-      it( 'should return a simple topic', function(){
-
-
-        var topic = manuh._createTopic('topic_1');
-        assert.equal(topic.name, 'topic_1');
-        assert.equal(topic.subscriptions.length, 0);
-      });
-      it( 'should create a topic with parent and relate them in both topics (parent and child)', function(){
-        var topic1 = manuh._createTopic('topic_1');
-        var topic2 = manuh._createTopic('topic_2', topic1);
-        assert(topic1.topic_2);
-        assert.equal(topic1.topic_2, topic2);
-        assert.equal(topic1.topic_2.name, 'topic_2');
-        assert.equal(topic2.parent, topic1);
-      });
+    beforeEach(function() {
+        manuh.topicsTree = {}; //manual reset
     });
 
-    describe('manuh._resolveTopicsByPath()', function() {
+    describe('topic creation', function() {
+        describe('manuh._createTopic()', function() {
 
-      it ('should return null', function() {
-        var topics = manuh._resolveTopicsByPath('');
-        assert.equal(topics, null);
-      });
-      it ('should return an array with 1 topic created based on a simple name (path)', function() {
-        var topics = manuh._resolveTopicsByPath('simple_topic');
-        assert.equal(topics.length, 1);
-        assert.equal(topics[0].name, 'simple_topic');
-        assert.equal(topics[0].parent, manuh.topicsTree);
-      });
-      it ('should return an array with 2 topics created based on a the name (charol/manuh)', function() {
-        var topics = manuh._resolveTopicsByPath('charol/manuh');
-        assert.equal(topics.length, 2);
-        assert.equal(topics[0].name, 'charol');
-        assert.equal(topics[1].name, 'manuh');
+            it( 'should return a simple topic', function(){
+                var topic = manuh._createTopic('topic_1');
+                assert.equal(topic.name, 'topic_1');
+                assert.equal(topic.subscriptions.length, 0);
+            });
+            it( 'should create a topic with parent and relate them in both topics (parent and child)', function(){
+                var topic1 = manuh._createTopic('topic_1');
+                var topic2 = manuh._createTopic('topic_2', topic1);
+                assert(topic1.topic_2);
+                assert.equal(topic1.topic_2, topic2);
+                assert.equal(topic1.topic_2.name, 'topic_2');
+                assert.equal(topic2.parent, topic1);
+            });
+        });
 
-        assert.equal(topics[0].manuh, topics[1]);
+        describe('manuh._resolveTopicsByPathRegex()', function() {
 
-        assert.equal(topics[0].parent, manuh.topicsTree);
-        assert.equal(topics[1].parent, topics[0]);
-      });
-      it ('should return an array with 3 topics created based on a the name (charol/manuh/rhelena)', function() {
-        var topics = manuh._resolveTopicsByPath('charol/manuh/rhelena');
-        assert.equal(topics.length, 3);
-        assert.equal(topics[0].name, 'charol');
-        assert.equal(topics[1].name, 'manuh');
-        assert.equal(topics[2].name, 'rhelena');
+            it ('should return null', function() {
+                var topics = manuh._resolveTopicsByPathRegex('');
+                assert.equal(topics, null);
+            });
+            it ('should return an array with 1 topic created based on a simple name (path)', function() {
+                var topics = manuh._resolveTopicsByPathRegex('simple_topic');
+                assert.equal(topics.length, 1);
+                assert.equal(topics[0].name, 'simple_topic');
+                assert.equal(topics[0].parent, manuh.topicsTree);
+            });
+            it ('should return an array with 2 topics created based on a the name (charol/manuh)', function() {
+                var topics = manuh._resolveTopicsByPathRegex('charol/manuh');
+                assert.equal(topics.length, 2);
+                assert.equal(topics[0].name, 'charol');
+                assert.equal(topics[1].name, 'manuh');
 
-        assert.equal(topics[0].manuh, topics[1]);
-        assert.equal(topics[1].rhelena, topics[2]);
+                assert.equal(topics[0].manuh, topics[1]);
 
-        assert.equal(topics[0].parent, manuh.topicsTree);
-        assert.equal(topics[1].parent, topics[0]);
-        assert.equal(topics[2].parent, topics[1]);
-      });
+                assert.equal(topics[0].parent, manuh.topicsTree);
+                assert.equal(topics[1].parent, topics[0]);
+            });
+            it ('should return an array with 3 topics created based on a the name (charol/manuh/rhelena)', function() {
+                var topics = manuh._resolveTopicsByPathRegex('charol/manuh/rhelena');
+                assert.equal(topics.length, 3);
+                assert.equal(topics[0].name, 'charol');
+                assert.equal(topics[1].name, 'manuh');
+                assert.equal(topics[2].name, 'rhelena');
+
+                assert.equal(topics[0].manuh, topics[1]);
+                assert.equal(topics[1].rhelena, topics[2]);
+
+                assert.equal(topics[0].parent, manuh.topicsTree);
+                assert.equal(topics[1].parent, topics[0]);
+                assert.equal(topics[2].parent, topics[1]);
+            });
+        });
+
     });
 
-  });
+    describe('topic find', function() {
+        describe('manuh._resolveTopic()', function() {
 
-  describe('topic find', function() {
-    describe('manuh._evaluateTopics()', function() {
+            it('should return all the topics that matches the simple regex (charol/manuh)', function() {
+                var topics = manuh._resolveTopic('charol/manuh');
+                assert.equal(topics.length, 1);
 
-      it('should return all the topics that matches the simple regex (charol/manuh)', function() {
-        var topics = manuh._evaluateTopics('charol/manuh');
-        assert.equal(topics.length, 1);
-
-        assert.equal(topics[0].name, 'manuh');
-        assert.equal(topics[0].parent.name, 'charol');
-      })
+                assert.equal(topics[0].name, 'manuh');
+                assert.equal(topics[0].parent.name, 'charol');
+            });
+        });
     });
-  });
 
-  describe('topic publish', function() {
-    describe('without subscriptions', function() {
-      describe('manuh.publish()', function() {
+    describe('topic publish', function() {
+        describe('manuh.publish()', function() {
 
-          it ('should create the topics based on the path to publish passed (charol/manuh/rhelena)', function() {
-            manuh.publish('charol/manuh/rhelena', '3 little girls!');
+            it ('should create the topics based on the path to publish passed (charol/manuh/rhelena)', function() {
+                manuh.publish('charol/manuh/rhelena', '3 little girls!');
 
-            assert(manuh.topicsTree.charol);
-            assert(manuh.topicsTree.charol.manuh);
-            assert(manuh.topicsTree.charol.manuh.rhelena);
+                assert(manuh.topicsTree.charol);
+                assert(manuh.topicsTree.charol.manuh);
+                assert(manuh.topicsTree.charol.manuh.rhelena);
 
-            assert.equal(manuh.topicsTree.charol.name, 'charol');
-            assert.equal(manuh.topicsTree.charol.manuh.name, 'manuh');
-            assert.equal(manuh.topicsTree.charol.manuh.rhelena.name, 'rhelena');
+                assert.equal(manuh.topicsTree.charol.name, 'charol');
+                assert.equal(manuh.topicsTree.charol.manuh.name, 'manuh');
+                assert.equal(manuh.topicsTree.charol.manuh.rhelena.name, 'rhelena');
 
-            assert.equal(manuh.topicsTree.charol.parent, manuh.topicsTree);
-            assert.equal(manuh.topicsTree.charol.manuh.parent, manuh.topicsTree.charol);
-            assert.equal(manuh.topicsTree.charol.manuh.rhelena.parent, manuh.topicsTree.charol.manuh);
+                assert.equal(manuh.topicsTree.charol.parent, manuh.topicsTree);
+                assert.equal(manuh.topicsTree.charol.manuh.parent, manuh.topicsTree.charol);
+                assert.equal(manuh.topicsTree.charol.manuh.rhelena.parent, manuh.topicsTree.charol.manuh);
 
-            assert.equal(Object.keys(manuh.topicsTree).length, 1);
-          });
-          it ('should create and modify the topics based on the path to publish passed (charol/manuh/rhelena)', function() {
-            manuh.publish('charol', '1 little girl!');
-            manuh.publish('charol/manuh', '2 little girls!');
-            manuh.publish('charol/manuh/rhelena', '3 little girls!');
+                assert.equal(Object.keys(manuh.topicsTree).length, 1);
+            });
+            it ('should create and modify the topics based on the path to publish passed (charol/manuh/rhelena)', function() {
+                manuh.publish('charol', '1 little girl!');
+                manuh.publish('charol/manuh', '2 little girls!');
+                manuh.publish('charol/manuh/rhelena', '3 little girls!');
 
-            assert(manuh.topicsTree.charol);
-            assert(manuh.topicsTree.charol.manuh);
-            assert(manuh.topicsTree.charol.manuh.rhelena);
+                assert(manuh.topicsTree.charol);
+                assert(manuh.topicsTree.charol.manuh);
+                assert(manuh.topicsTree.charol.manuh.rhelena);
 
-            assert.equal(manuh.topicsTree.charol.name, 'charol');
-            assert.equal(manuh.topicsTree.charol.manuh.name, 'manuh');
-            assert.equal(manuh.topicsTree.charol.manuh.rhelena.name, 'rhelena');
+                assert.equal(manuh.topicsTree.charol.name, 'charol');
+                assert.equal(manuh.topicsTree.charol.manuh.name, 'manuh');
+                assert.equal(manuh.topicsTree.charol.manuh.rhelena.name, 'rhelena');
 
-            assert.equal(manuh.topicsTree.charol.parent, manuh.topicsTree);
-            assert.equal(manuh.topicsTree.charol.manuh.parent, manuh.topicsTree.charol);
-            assert.equal(manuh.topicsTree.charol.manuh.rhelena.parent, manuh.topicsTree.charol.manuh);
+                assert.equal(manuh.topicsTree.charol.parent, manuh.topicsTree);
+                assert.equal(manuh.topicsTree.charol.manuh.parent, manuh.topicsTree.charol);
+                assert.equal(manuh.topicsTree.charol.manuh.rhelena.parent, manuh.topicsTree.charol.manuh);
 
-            assert(!manuh.topicsTree.romeu);
-            manuh.publish('romeu', '1 funny boy!');
-            assert(manuh.topicsTree.romeu);
+                assert(!manuh.topicsTree.romeu);
+                manuh.publish('romeu', '1 funny boy!');
+                assert(manuh.topicsTree.romeu);
 
-            assert.equal(Object.keys(manuh.topicsTree).length, 2);
-          });
+                assert.equal(Object.keys(manuh.topicsTree).length, 2);
+            });
 
-      });
-    }); //without subscriptions
+        });
 
-    describe('with subscriptions', function() {
-      describe('manuh.publish()', function() {
-        it('should fire the subscription on the simple topic', function() {
-          var _sentMessage = '1 little girl!';
-          manuh.subscribe('charol', function(msg){
-            assert.equal(msg, _sentMessage)
-          });
-        })
-      });
-    }); //with subscriptions
+        describe('topic subscription', function() {
+            describe('manuh.subscribe()', function() {
 
-  });
+                it ('should create the topics based on the path to subscribe passed (charol/manuh/rhelena)', function() {
+                    manuh.subscribe('charol/manuh/rhelena', function(msg){});
+
+                    assert(manuh.topicsTree.charol);
+                    assert(manuh.topicsTree.charol.manuh);
+                    assert(manuh.topicsTree.charol.manuh.rhelena);
+
+                    assert.equal(manuh.topicsTree.charol.name, 'charol');
+                    assert.equal(manuh.topicsTree.charol.manuh.name, 'manuh');
+                    assert.equal(manuh.topicsTree.charol.manuh.rhelena.name, 'rhelena');
+
+                    assert.equal(manuh.topicsTree.charol.parent, manuh.topicsTree);
+                    assert.equal(manuh.topicsTree.charol.manuh.parent, manuh.topicsTree.charol);
+                    assert.equal(manuh.topicsTree.charol.manuh.rhelena.parent, manuh.topicsTree.charol.manuh);
+
+                    assert.equal(Object.keys(manuh.topicsTree).length, 1);
+                });
+                it ('should create and modify the topics based on the path to publish passed (charol/manuh/rhelena)', function() {
+                    manuh.subscribe('charol', function(msg){});
+                    manuh.subscribe('charol/manuh', function(msg){});
+                    manuh.subscribe('charol/manuh/rhelena', function(msg){});
+
+                    assert(manuh.topicsTree.charol);
+                    assert(manuh.topicsTree.charol.manuh);
+                    assert(manuh.topicsTree.charol.manuh.rhelena);
+
+                    assert.equal(manuh.topicsTree.charol.name, 'charol');
+                    assert.equal(manuh.topicsTree.charol.manuh.name, 'manuh');
+                    assert.equal(manuh.topicsTree.charol.manuh.rhelena.name, 'rhelena');
+
+                    assert.equal(manuh.topicsTree.charol.parent, manuh.topicsTree);
+                    assert.equal(manuh.topicsTree.charol.manuh.parent, manuh.topicsTree.charol);
+                    assert.equal(manuh.topicsTree.charol.manuh.rhelena.parent, manuh.topicsTree.charol.manuh);
+
+                    assert(!manuh.topicsTree.romeu);
+                    manuh.subscribe('romeu', function(msg){});
+                    assert(manuh.topicsTree.romeu);
+
+                    assert.equal(Object.keys(manuh.topicsTree).length, 2);
+                });
+
+            });
+        }); //with subscriptions
+
+    });
 
 });
