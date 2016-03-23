98722a6dbd3079896edd86a105e66897ae9fff5b
tiagostutz <tiagostutz@bb.com.br>
2016-03-22 21:09:29 -0300

    ignoring node_modules

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('lint', function() {
+    return gulp.src('manuh.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('manuh.js')
+        .pipe(concat('manuh.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('manuh.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('*.js', ['lint', 'scripts']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'scripts', 'watch']);
