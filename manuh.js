a1145d0239a1faa38855feb7ab6835337da050ed
flaviostutz <flaviostutz@gmail.com>
2017-11-15 21:02:22 -0200

    implementation of retained messages using 'memory' and 'localStorage' providers

--- manuh.js
+++ manuh.js
@@ -31,6 +31,7 @@ var _manuhData = {
     topicsTree : {},
 
     __publishCallbackInvokeIntervalDelay : 0, //mainly used for development porpuses
+
 }
 
 var _manuhFunctions = {
@@ -42,10 +43,11 @@ var _manuhFunctions = {
         var topic = {
             name : _name,
             parent : _parent,
+            retainedMessage: null,
             subscriptions : [],
             addSubscription: function(onMessageReceived) {
                 this.subscriptions.push({onMessageReceived : onMessageReceived});
-            }
+            },
         };
 
         if (_parent) {
@@ -53,6 +55,19 @@ var _manuhFunctions = {
         }
 
         return topic;
+    },
+
+    _getTopicPath: function __getTopicPath(topicNode) {
+      console.log('getTopicPath ' + JSON.stringify(topicNode, function(key, value) {
+        if(key == 'parent') {return value.id;}
+        else {return value;}
+      }));
+      if(topicNode.parent==null) {
+        return '';
+      } else {
+        let parentName = __getTopicPath(topicNode.parent);
+        return parentName + (parentName!=''?'/':'') + topicNode.name;
+      }
     },
 
     //returns an array with all matched topics
@@ -92,12 +107,39 @@ var _manuhFunctions = {
       }else{
           throw {msg: 'Error to resolve a topic by the path provided because it has a wildcard and hence could find 2 or more topcis. This method is intended to be used to get only one topic. To resolve path using wildcards use `_resolveTopicsByPathRegex`.'};
       }
+    },
+
+    _multicastMessage: function(topicToPublish, message) {
+      var invokeCallbackIsolated = function(subsc) {
+          var _subsc = subsc;
+          setTimeout(function() {
+              console.log('>>>>>> CALLING ONMESSAGE ');
+              _subsc.onMessageReceived(message);
+          }, _manuhData.__publishCallbackInvokeIntervalDelay);
+      };
+
+      for(var k=0; k<topicToPublish.subscriptions.length; k++) {
+          //invoke the callbacks asynchronously and with a closed scope
+          var subscription = topicToPublish.subscriptions[k];
+          console.log('>>>>>> INVOKE ' + k);
+          new invokeCallbackIsolated(subscription);
+      }
     }
 
+
 };
+
 module.exports = {
 
-    publish: function(topicPath, message) {
+    /*
+     * Publishes a message to a topic.
+     * options: {
+     *    retained: true  - whetever to keep this message and deliver to new subscribers as soon as they subscribe or not
+     *    retainment_provider: 'memory', 'localStorage' or another string registered with manuh.registerRetainProvider(name, function);
+     * }
+     */
+    publish: function(topicPath, message, options) {
+        console.log('PUBLISH ' + topicPath + '=' + JSON.stringify(message) + ' ' + JSON.stringify(options));
         var _self = this;
         var topicToPublish = null;
 
@@ -111,18 +153,22 @@ module.exports = {
             throw {msg: 'Error to publish message on topic because there were found more than 1 topic for the provied topicPath. You can publish only to one topic. Check if there are duplicated topic names.'};
         }
 
-        var invokeCallbackIsolated = function(subsc) {
-              var _subsc = subsc;
-              setTimeout(function() {
-                _subsc.onMessageReceived(message);
-            }, _manuhData.__publishCallbackInvokeIntervalDelay);
-        };
+        if(options && options.retained) {
 
-        for(var k=0; k<topicToPublish.subscriptions.length; k++) {
-            //invoke the callbacks asynchronously and with a closed scope
-            var subscription = topicToPublish.subscriptions[k];
-            new invokeCallbackIsolated(subscription);
+          if(!options.retainment_provider || options.retainment_provider=='memory') {
+            topicToPublish.retainedMessage = message;
+
+          } else if(options.retainment_provider=='localStorage') {
+            let key = '[manuh-retained]' + _manuhFunctions._getTopicPath(topicToPublish);
+            console.log('publish retained ' + key);
+            localStorage.setItem(key, JSON.stringify(message));
+
+          } else {
+            throw 'options.retainment_provider must be one of ["memory", "localStorage"]';
+          }
         }
+
+        _manuhFunctions._multicastMessage(topicToPublish, message);
 
     },
 
@@ -133,13 +179,31 @@ module.exports = {
 
       var topicToSubscribe = null;
 
+      console.log('subscribing to ' + topicPathRegex);
       if (!_manuhFunctions._hasSpecialWildcard(topicPathRegex)) {
           topicToSubscribe = _manuhFunctions._resolveTopic(topicPathRegex);
           topicToSubscribe.addSubscription(onMessageReceived);//if there aren't wildcards on the topicPath, them it will be a subscription for only one topic
-      }else{ //if the path has a wildcard that needs to be evaluated
+          console.log('TOPICSUB ' + JSON.stringify(topicToSubscribe.retainedMessage));
 
+          //lookup for retained messages in memory
+          if(topicToSubscribe.retainedMessage) {
+            console.log('multicasting retained message after subscription');
+            _manuhFunctions._multicastMessage(topicToSubscribe, topicToSubscribe.retainedMessage);
+
+          //lookup for retained messages on local-storage
+          } else {
+            let key = '[manuh-retained]' + _manuhFunctions._getTopicPath(topicToSubscribe);
+            console.log('subscribe retained ' + key);
+            var message = JSON.parse(localStorage.getItem(key));
+            if(message) {
+              _manuhFunctions._multicastMessage(topicToSubscribe, message);
+            }
+          }
+
+      } else { //if the path has a wildcard that needs to be evaluated
+        throw 'Wildcard paths not supported for subscriptions yet';
       }
-    }
+    },
 
 };
 module.exports.manuhData = _manuhData;
